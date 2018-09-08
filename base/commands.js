ff22a9c4e2c9c06d482b095e0296e261cb028b58
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-09-08 15:19:03 -0400

    Added command class and run method

--- base/commands.js
+++ base/commands.js
@@ -1,2 +1,19 @@
 class Command {
+  constructor(tempName, tempCooldown, tempCategory, tempDescription, tempFunction) {
+    this.name = tempName;
+    this.cooldown = tempCooldown;
+    this.category = tempCategory;
+    this.description = tempDescription;
+    this.function = tempFunction;
+    this.activeCooldown = false;
+  }
+  run() {
+    if(this.activeCooldown == false) {
+      this.function();
+      this.activeCooldown = true;
+      setTimeout(() => {
+            this.activeCooldown = false;
+        }, this.cooldown * 1000)
+    }
+  }
 }
