bf025e953717fe10c5ee3649ca42cef22179b493
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-09-08 19:19:32 -0400

    Added prefix exports

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,1 +1,17 @@
+import Command from '../base/commands.js';
 
+let prefix = new Command("prefix", 0, "admin", "Changes the guild command prefix." (tempArgs) => {
+  if(!tempArgs[0]) {
+    return message.reply("Not prefix input.");
+  }
+  let prefixes = JSON.parse(fs.readFileSync("../database/prefixes.json", "utf8"));
+  prefixes[message.guild.id] = {
+    prefix: tempArgs
+  };
+  fs.writeFileSync("../database/prefixes.json", JSON.stringify(prefixes,null,4), (err) => {
+        if (err) console.log(err)
+  });
+  message.reply("Prefix updated);
+});
+  
+export prefix;
