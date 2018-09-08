7b52173c0f75953f77eae5a29a4210a3df1fa2d9
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-09-08 18:58:49 -0400

    added command export

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,1 +1,16 @@
+import Command from '../base/commands.js';
 
+let botinfo = new Command("botinfo", 30, "info", "Shows ping and bot information.", () => {
+  let boticon = bot.user.displayAvatarURL;
+  let embed = new Discord.RichEmbed()
+    .setTitle("Bot Information")
+    .setColor("#8e278e")
+    .setThumbnail(bot.user.displayAvatarURL)
+    .addField("Server Count", bot.guilds.size)
+    .addField("User Count", bot.guilds.users.size)
+    .addField("Ping", (Math.round(bot.ping) + " ms"));
+
+    message.channel.send(embed);
+});
+
+export default botinfo;
