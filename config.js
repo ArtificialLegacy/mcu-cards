7aff36ac1f53f8eec4cee70e57a0f6cc888932a5
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-09-09 16:28:44 -0400

    Added bot + updated name

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 const Discord = require("discord.js");
 const fs = require("fs");
+const bot = new Discord.Client({disableEveryone: true});
 
 let settings = {
   Discord: Discord,
-  fs: fs
+  fs: fs,
+  bot: bot
 };
 
 module.exports = settings;
