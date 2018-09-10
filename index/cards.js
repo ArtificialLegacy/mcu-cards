1ea6183b0b8ed648b7b085c5c40a77c37fd7d49c
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-09-09 20:50:21 -0400

    changed array to object

--- index/cards.js
+++ index/cards.js
@@ -2,15 +2,15 @@ let items = require("../base/items.js");
 let Card = items.Card;
 
 const cards = {
-  characters: [
-    new Card("Thor", "The god of thunder.", "http://www.buffed.de/screenshots/original/2016/07/Chris-Hemsworth-Thor-Ragnarok-pc-games1.jpg", 75, "Epic"),
-    new Card("Thor", "The lord of thunder.", "https://media0dk-a.akamaihd.net/63/86/6efba9b0e336f748410100dd500c6f66.jpg", 80, "Epic"),
-    new Card("The Hulk", "Hulk Smash!", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX4t3_-BQRuz_d_SfETCGN2zVIBPuE_fC8iL8bzchaH4Asut9H", 20, "Uncommon"),
-    new Card("The Hulk", "Hulk Smash!", "https://media.wired.com/photos/59b83283b412f33ba9daa3f5/master/pass/thehulk-FA.jpg", 25, "Uncommon"),
-    new Card("The Grandmaster", "Time works real different around these parts.", "https://upload.wikimedia.org/wikipedia/en/thumb/0/00/Jeff_Goldblum_as_the_Grandmaster_in_Thor_Ragnarok.jpg/170px-Jeff_Goldblum_as_the_Grandmaster_in_Thor_Ragnarok.jpg", 50 "Rare"),
-    new Card("Spiderman", "Hey everyone.", "https://movietvtechgeeks.com/wp-content/uploads/2017/07/spiderman-homecoming-great-reviews-lands.jpg", 40, "Rare"),
-  ],
-  items: [
+  characters: {
+    "Thor1": new Card("Thor", "The god of thunder.", "http://www.buffed.de/screenshots/original/2016/07/Chris-Hemsworth-Thor-Ragnarok-pc-games1.jpg", 75, "Epic"),
+    "Thor2": new Card("Thor", "The lord of thunder.", "https://media0dk-a.akamaihd.net/63/86/6efba9b0e336f748410100dd500c6f66.jpg", 80, "Epic"),
+    "Hulk1": new Card("The Hulk", "Hulk Smash!", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX4t3_-BQRuz_d_SfETCGN2zVIBPuE_fC8iL8bzchaH4Asut9H", 20, "Uncommon"),
+    "Hulk2": new Card("The Hulk", "Hulk Smash!", "https://media.wired.com/photos/59b83283b412f33ba9daa3f5/master/pass/thehulk-FA.jpg", 25, "Uncommon"),
+   "Grandmaster1": new Card("The Grandmaster", "Time works real different around these parts.", "https://upload.wikimedia.org/wikipedia/en/thumb/0/00/Jeff_Goldblum_as_the_Grandmaster_in_Thor_Ragnarok.jpg/170px-Jeff_Goldblum_as_the_Grandmaster_in_Thor_Ragnarok.jpg", 50 "Rare"),
+    "Spiderman": new Card("Spiderman", "Hey everyone.", "https://movietvtechgeeks.com/wp-content/uploads/2017/07/spiderman-homecoming-great-reviews-lands.jpg", 40, "Rare"),
+  },
+  items: {
     
-  ]
+  }
 }
