8641fa5aba48fe65369b8f4d612550a40a204d8b
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-09-09 20:06:09 -0400

    Added enmap setup

--- config.js
+++ config.js
@@ -2,10 +2,15 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
 
+const Enmap = require("enmap");
+const database = new Enmap({name: "points"});
+
 let settings = {
   Discord: Discord,
   fs: fs,
-  bot: bot
+  bot: bot,
+  Enmap: Enmap,
+  database: database
 };
 
 module.exports = settings;
