25b7cd59910f9359c4a59235f46e92d2701eec84
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-09-11 18:23:44 -0400

    Added enmap leve

--- config.js
+++ config.js
@@ -2,15 +2,21 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
 
-const Enmap = require("enmap");
-const database = new Enmap({name: "points"});
+const Enmap = require('enmap');
+const EnmapLevel = require('enmap-level');
+const data = new Enmap({
+  provider: new EnmapLevel({
+    name: 'data' 
+  });
+});
 
 let settings = {
   Discord: Discord,
   fs: fs,
   bot: bot,
   Enmap: Enmap,
-  database: database
+  EnmapLevel: EnmapLevel,
+  data: data
 };
 
 module.exports = settings;
