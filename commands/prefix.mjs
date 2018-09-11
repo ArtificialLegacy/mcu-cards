3b9153aba5ecb44f9dcfddb1de55bdfba1949781
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-09-11 19:26:06 -0400

    updated to databasing

--- commands/prefix.mjs
+++ commands/prefix.mjs
@@ -1,20 +1,17 @@
-import Command from '../base/commands.js';
+import Command from '../base/commands.mjs';
 import settings from '../config.js';
 const Discord = settings.Discord;
 const bot = settings.bot;
 const fs = settings.fs;
+const data = settings.data;
 
 let prefix = new Command("prefix", 0, "admin", "Changes the guild command prefix.", (tempMessage, tempArgs, tempPrefix) => {
-  if(!tempArgs[0]) {
+if(!tempArgs[0]) {
     return tempMessage.reply("Not prefix input.");
   }
-  let prefixes = JSON.parse(fs.readFileSync("../database/prefixes.json", "utf8"));
-  prefixes[message.guild.id] = {
-    prefix: tempArgs
-  };
-  fs.writeFileSync("../database/prefixes.json", JSON.stringify(prefixes,null,4), (err) => {
-        if (err) console.log(err)
-  });
+  let prefix = data.get(`${tempMessage.guild.id}.prefix`);
+  data.set(`${tempMessage.guild.id}.prefix`, "!");
+
   tempMessage.reply("Prefix updated");
 });
   
