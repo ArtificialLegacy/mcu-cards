e9cc43c5e8325e49f1dcdf69fbfda69d3db659b1
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-09-11 07:33:32 -0400

    Update commandExport.mjs

--- base/commandExport.mjs
+++ base/commandExport.mjs
@@ -1,1 +1,9 @@
+import botinfo from '../commands/botinfo.mjs';
+import prefix from '../commands/prefix.mjs';
+import help from '../commands/help.mjs';
 
+const commands = {
+  botinfo: botinfo,
+  prefix: prefix,
+  help: help,
+}
