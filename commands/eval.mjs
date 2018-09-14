6e7136947b33a5b7f68f44ee001a91b75cc5d3d9
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-09-14 16:36:51 -0400

    Update eval.mjs

--- commands/eval.mjs
+++ commands/eval.mjs
@@ -1,1 +1,22 @@
+import setting from '../config.js';
+const Discord = settings.Discord;
+const fs = settings.fs;
+const data = settings.data;
+const bot = settings.bot;
 
+
+
+let debug = new Command("debug", 0, "admin", "Bot debugging command. (Devs only)", (tempMessage, tempArgs, tempPrefix) => {
+  if(!message.author.id === "261619158096150528") return message.reply("You can not use this command!");
+
+    let value = (eval(args.join(" ")));
+
+    let embed = new Discord.RichEmbed()
+    .setDescription("Eval")
+    .setColor("#8e278e")
+    .addField("Requested User", message.author)
+    .addField("Input", args)
+    .addField("Result", value);
+
+    message.channel.send(embed);
+});
