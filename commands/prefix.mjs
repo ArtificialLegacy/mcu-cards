c4c1f6735c8a7195cc8363a953bcceb3a70be1d7
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-09-14 15:07:04 -0400

    fixed indenting

--- commands/prefix.mjs
+++ commands/prefix.mjs
@@ -6,13 +6,14 @@ const fs = settings.fs;
 const data = settings.data;
 
 let prefix = new Command("prefix", 0, "admin", "Changes the guild command prefix.", (tempMessage, tempArgs, tempPrefix) => {
-if(!tempArgs[0]) {
-    return tempMessage.reply("Not prefix input.");
-  }
-  let prefix = data.get(`${tempMessage.guild.id}.prefix`);
-  data.set(`${tempMessage.guild.id}.prefix`, "!");
+    if(!tempArgs[0]) {
+        return tempMessage.reply("Not prefix input.");
+    }
+    
+    let prefix = data.get(`${tempMessage.guild.id}.prefix`);
+    data.set(`${tempMessage.guild.id}.prefix`, "!");
 
-  tempMessage.reply("Prefix updated");
+    tempMessage.reply("Prefix updated");
 });
   
 export default prefix;
