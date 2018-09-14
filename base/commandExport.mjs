a0d7269e51c026b24b72277b913c34717435e3f2
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-09-14 16:39:36 -0400

    added debug command

--- base/commandExport.mjs
+++ base/commandExport.mjs
@@ -1,11 +1,13 @@
 import botinfo from '../commands/botinfo.mjs';
 import prefix from '../commands/prefix.mjs';
 import help from '../commands/help.mjs';
+import debug from '../commands/eval.mjs';
 
 const commands = {
   botinfo: botinfo,
   prefix: prefix,
   help: help,
+  debug: debug,
 }
 
 export default commands;
