3fe695de5557249957ab1e5748a867db6acfffcb
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-09-14 20:01:08 -0400

    fixed typo

--- commands/eval.mjs
+++ commands/eval.mjs
@@ -7,18 +7,18 @@ const bot = settings.bot;
 import Command from '../base/commands.mjs';
 
 let debug = new Command("debug", 0, "admin", "Bot debugging command. (Devs only)", (tempMessage, tempArgs, tempPrefix) => {
-  if(!message.author.id === "261619158096150528") return message.reply("You can not use this command!");
+  if(!tempMessage.author.id === "261619158096150528") return tempMessage.reply("You can not use this command!");
 
     let value = (eval(args.join(" ")));
 
     let embed = new Discord.RichEmbed()
     .setDescription("Eval")
     .setColor("#8e278e")
-    .addField("Requested User", message.author)
+    .addField("Requested User", tempMessage.author)
     .addField("Input", tempArgs)
     .addField("Result", value);
 
-    message.channel.send(embed);
+    tempMessage.channel.send(embed);
 });
 
 export default debug;
