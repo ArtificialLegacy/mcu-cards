9d426585ab6efa47e7e7c11e16cb79bad1110d44
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-09-15 18:29:22 -0400

    Added more cards

--- index/cards.mjs
+++ index/cards.mjs
@@ -13,7 +13,14 @@ const cards = {
     "Thanos": new Card("Thanos", "Perfectly balanced, as all things should be.", "https://www.sideshowtoy.com/wp-content/uploads/2018/04/marvel-avengers-infinity-war-thanos-sixth-scale-figure-hot-toys-feature-903429-1.jpg", 100, "Legendary"),
     "DoctorStrange": new Card("Doctor Strange", "I don't believe in fairy tales about chakras or energy or the power of belief.", "https://images.spot.im/v1/production/p6yd1fpcoekrg3sofm5e", 40, "Rare"),
     "AncientOne": new Card("Ancient One", "What if I told you that reality is one of many?", "https://www.usajacket.com/blog/wp-content/uploads/2018/08/Doctor-Strange-The-Ancient-One-Costume.jpg", 35, "Uncommon"),
-    "Gamora": new Card("Gamora", "I am going to die surrounded by the biggest idiots in the galaxy.", "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/6/61/Gamora_AIW_Profile.jpg/revision/latest/scale-to-width-down/310?cb=20180518212221", 55, "Rare")
+    "Gamora": new Card("Gamora", "I am going to die surrounded by the biggest idiots in the galaxy.", "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/6/61/Gamora_AIW_Profile.jpg/revision/latest/scale-to-width-down/310?cb=20180518212221", 55, "Rare"),
+    "StarLord": new Card("Star Lord", "You said it yourself. We're the Guardians of the Galaxy.", "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/9/96/Star-Lord_AIW_Profile.jpg/revision/latest/scale-to-width-down/310?cb=20180518211942", 50, "Rare"),
+    "BlackWidow": new Card("Black Widow", "Hey, big guy. Sun's getting real low.", "https://ksassets.timeincuk.net/wp/uploads/sites/55/2018/02/Black-Widow-Avengers-920x584.jpg", 20, "Common"),
+    "Drax": new Card("Drax", "There are two types of beings in the universe. Those who dance, and those who do not.", "https://images.indianexpress.com/2018/05/avengers-drax.jpg", 40, "Uncommon"),
+    "CaptainAmerica": new Card("Captain America", "We don't trade lives here", "https://i.pinimg.com/originals/99/fd/97/99fd97e6a3ab69932785110bebb7e411.jpg", 60, "Rare"),
+    "CaptainAmerica2": new Card("Captain America", "We don't trade lives here", "http://moviehole.net/img/captain-america-avengers-infinity-war.jpg", 65, "Rare"),
+    "Hawkeye": new Card("Hawkeye", "What? You didn't see that coming?", "https://cdn.flickeringmyth.com/wp-content/uploads/2017/08/hawkeye-jeremy-renner-600x338.jpg", 15, "Common"),
+    "Odin": new Card("Odin", "Malekith was vanquished and the Aether was no more, or so we were led to beleive...", "http://digitalspyuk.cdnds.net/18/24/980x490/landscape-1528905527-odin-thor-cornation.jpg", 75, "Epic"),
   },
   items: {
     
