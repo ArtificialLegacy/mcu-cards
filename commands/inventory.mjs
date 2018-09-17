e28b65bd226231e12d483854b6a5754210b50f68
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-09-17 12:40:36 -0400

    Added command setup

--- commands/inventory.mjs
+++ commands/inventory.mjs
@@ -1,1 +1,12 @@
+import settings from '../config.js';
+const Discord = settings.Discord;
+const fs = settings.fs;
+const data = settings.data;
 
+import Command from '../base/commands.mjs';
+
+let inventory = new Command("inventory", 10, "cards", "Opens your card inventory.", (tempMessage, tempArgs, tempPrefix) => {
+  
+});
+
+export default inventory;
