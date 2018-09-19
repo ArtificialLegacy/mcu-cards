60d78852a174ed2fe664dd6a9d24f7c110e401e2
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-09-19 17:13:22 -0400

    added inventory

--- base/commandExport.mjs
+++ base/commandExport.mjs
@@ -2,12 +2,14 @@ import botinfo from '../commands/botinfo.mjs';
 import prefix from '../commands/prefix.mjs';
 import help from '../commands/help.mjs';
 import debug from '../commands/eval.mjs';
+import inventory from '../commands/inventory.mjs';
 
 const commands = {
   botinfo: botinfo,
   prefix: prefix,
   help: help,
   debug: debug,
+  inventory: inventory,
 }
 
 export default commands;
