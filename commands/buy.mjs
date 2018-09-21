fbff36712a4473f5a222a92c388c3363bec71f86
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-09-21 12:11:41 -0400

    added base command

--- commands/buy.mjs
+++ commands/buy.mjs
@@ -1,1 +1,15 @@
+import settings from '../config.js';
+const Discord = settings.Discord;
+const data = settings.data;
+const bot = settings.bot;
 
+import Command from '../base/commands.mjs';
+import cards from '../index/cards.mjs';
+import packs from '../index/packs.mjs';
+import sets from '../index/sets.mjs';
+
+let buy = new Command("buy", 10, "Open the shop and buy items", (tempMessage, tempArgs, tempPrefix) => {
+  
+});
+
+export default buy;
