18638a217f2ae7a1abe8a1d1c7bc7459019663a1
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-09-21 12:19:19 -0400

    added argument handling

--- commands/buy.mjs
+++ commands/buy.mjs
@@ -8,8 +8,19 @@ import cards from '../index/cards.mjs';
 import packs from '../index/packs.mjs';
 import sets from '../index/sets.mjs';
 
-let buy = new Command("buy", 10, "Open the shop and buy items", (tempMessage, tempArgs, tempPrefix) => {
-  
+let buy = new Command("buy", 10, "cards", "Open the shop and buy items", (tempMessage, tempArgs, tempPrefix) => {
+  if(!tempArgs[0]){
+    tempMessage.reply("No subcommand input! (view, buy)");
+    return;
+  }
+  if(tempArgs[0] == "view"){
+    
+  } else if(tempArgs[0] == "buy"){
+    
+  } else {
+    tempMessage.reply("Invalid subcommand! (view, buy)");
+    return;
+  }
 });
 
 export default buy;
